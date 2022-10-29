6a70f885bbeadf8ce641903f0625241c304a6343
Quentin RIDEL <quentin.ridel@etu.univ-rouen.fr>
2022-10-29 12:06:28 +0200

    feat: added GET /shop endpoint

--- routes/shop.js
+++ routes/shop.js
@@ -4,5 +4,24 @@ const express = require('express');
 const mysqlConnection = require("../connection");
 const router = express.Router();
 
+//préparation des requêtes
+const query_getShops = "SELECT * from shops;";
+
+
+//obtenir tout les magasins
+router.get("/shop", (req, res) => {
+    mysqlConnection.query(query_getShops, (err, rows, fields)=>{
+        if(!err){
+            if (res.length = 0) {
+                res.status(204).send(rows);
+            }
+            res.status(200).send(rows);
+        }
+        else {
+            res.status(500).send(err);
+        }
+
+    })
+});
 
 module.exports = router;
