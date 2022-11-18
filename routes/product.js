747e32867de8b358bbf8de3512d6d4862fca42aa
Quentin RIDEL <quentin.ridel@etu.univ-rouen.fr>
2022-11-18 16:21:47 +0100

    feat: added post on /product to add product in db

--- routes/product.js
+++ routes/product.js
@@ -5,5 +5,26 @@ const express = require('express');
 const mysqlConnection = require("../connection");
 const router = express.Router();
 
+//préparation des requêtes
+const query_insertProduct= "insert into Products(name, price) values (?,?);"
+
+
+//ajouter un nouveau produit
+router.post("/product",(req, res) => {
+    try { 
+        mysqlConnection.query(query_insertProduct, 
+            [req.body.name, req.body.price],
+            (err, rows, fields)=>{
+            if(!err){
+                res.status(201).send("Le produit à été ajouté");;
+            } else {
+                res.status(500).send("Impossible de créer ce produit, veuillez entrer des données correctes");
+            }
+        }); 
+    } catch {
+        res.status(500).send('Erreur');
+    }          
+});
+
 
 module.exports = router;
