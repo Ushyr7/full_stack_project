34fe70a1dd02a39c893caf6a7ea02956bd9e2d00
Quentin RIDEL <quentin.ridel@etu.univ-rouen.fr>
2022-11-18 16:50:22 +0100

    feat: added put on /product/:id to update a product with its id

--- routes/product.js
+++ routes/product.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 //préparation des requêtes
 const query_insertProduct= "insert into Products(name, price) values (?,?);"
-
+const query_updateProduct= "update Products set name = ?, price = ?, description = ? where id = ?; "
 
 //ajouter un nouveau produit
 router.post("/product",(req, res) => {
@@ -26,5 +26,23 @@ router.post("/product",(req, res) => {
     }          
 });
 
+//mettre un jour un produit avec son id
+router.put("/product/:id",(req, res) => {
+    try { 
+        mysqlConnection.query(query_updateProduct, 
+            [req.body.name, req.body.price, req.body.description, req.params.id],
+            (err, result)=>{
+            if(err){
+                res.status(500).send("Impossible de modifier le produit "+ req.params.id + ", veuillez entrer des données correctes");
+            }
+            if(result.affectedRows == 0) {
+                res.status(404).send("Impossible de trouver le produit " + req.params.id)
+            }
+            res.status(201).send("Le produit " + req.params.id + " a été modifié");
+        }); 
+    } catch {
+        res.status(500).send('Erreur');
+    }          
+});
 
 module.exports = router;
