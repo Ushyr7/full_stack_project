c5910a4e9089f9ef988f89f568f95404a6f2d743
Quentin RIDEL <quentin.ridel@etu.univ-rouen.fr>
2022-11-19 12:15:16 +0100

    feat: added delete on /shop/:id to delete a shop with its id, added put on /shop/:id to update a shop with its id

--- routes/shop.js
+++ routes/shop.js
@@ -9,6 +9,8 @@ const query_getShop = "SELECT shops.id, name, isAvailable, created from shops;";
 const query_insertShop= "insert into Shops(name, isAvailable, created, creatorId) values (?,?, NOW(), ?);"
 const query_getNewShopId= "SELECT LAST_INSERT_ID() as id;"
 const query_insertSchedule= "insert into Schedule(shopId, day, open, close) values (?, ?, ?, ?);"
+const query_deleteShop= "delete from Shops where id = ?;"
+const query_updateShop= "update Shops set name = ?, isAvailable = ? where id = ?;"
 
 //obtenir tout les magasins
 router.get("/shop", (req, res) => {
@@ -49,6 +51,46 @@ router.post("/shop",(req, res) => {
         });
     } catch {
         res.status(500).send("Erreur");
+    }          
+});
+
+//mettre un jour un magasin avec son id
+router.put("/shop/:id",(req, res) => {
+    try { 
+        mysqlConnection.query(query_updateShop, 
+            [req.body.name, req.body.isAvailable, req.params.id],
+            (err, result)=>{
+            if(err){
+                res.status(500).send("Impossible de modifier le magasin "+ req.params.id + ", veuillez entrer des données correctes");
+            }
+            else if(result.affectedRows == 0) {
+                res.status(404).send("Impossible de trouver le magasin " + req.params.id)
+            } else {
+                res.status(201).send("Le magasin " + req.params.id + " a été modifiée");
+            }
+        }); 
+    } catch {
+        res.status(500).send('Erreur');
+    }          
+});
+
+//supprimer un magasin avec son id
+router.delete("/shop/:id",(req, res) => {
+    try { 
+        mysqlConnection.query(query_deleteShop, 
+            [req.params.id],
+            (err, result)=>{
+            if(err){
+                res.status(500).send("Impossible de supprimer le magasin "+ req.params.id + ", veuillez entrer des données correctes");
+            }
+            else if(result.affectedRows == 0) {
+                res.status(404).send("Impossible de trouver le magasin " + req.params.id)
+            } else {
+                res.status(200).send("Le magasin " + req.params.id + " a été supprimé");
+            }
+        }); 
+    } catch {
+        res.status(500).send('Erreur');
     }          
 });
 
